82573b091d4954549a1bd1c097c640e6388be53c
raitaskeen <raitaskeenhaider786@gmail.com>
2024-10-31 11:41:38 +0500

    Add basic variable examples in Python

---
+++ 01_Basics/01_variables.py
@@ -0,0 +1,26 @@
+# Variables in Python
+
+# What is a Variable?
+#A variable is like a box where you can store a value. You can give this box a name,
+#and then you can use the name to get the value from the box whenever you need it.
+
+# 
+# How to create a variable?
+# In Python, you can create a variable by giving it a name and assigning a value to it.
+# The assignment operator in Python is the equal sign (=).
+
+# Storing a number
+age = 10
+print("Age:", age)
+
+# Storing a word
+name = "Alice"
+print("Name:", name)
+
+# Storing a decimal number
+height = 4.5
+print("Height:", height)
+
+# Storing a true/false value
+is_student = True
+print("Is student:", is_student)
