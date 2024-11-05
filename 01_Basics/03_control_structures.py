2827b11c5d9036c9d4e6a2f2a5f1096a229b6b01
raitaskeen <raitaskeenhaider786@gmail.com>
2024-11-05 15:15:03 +0500

    Add examples of control structures in Python: if-else, for loop, and while loop

---
+++ 01_Basics/03_control_structures.py
@@ -0,0 +1,20 @@
+# Control Structures in Python
+
+# If-Else Statement Example
+age = int(input("Enter your age: "))
+if age < 13:
+    print("You are a child.")
+elif age < 20:
+    print("You are a teenager.")
+else:
+    print("You are an adult.")
+
+# For Loop Example
+for i in range(1, 6):
+    print(i)
+
+# While Loop Example
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
